f305385232ae233100bfbfdbb2a6f38b173e3441
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-07-23 00:29:31 +0530

    Boilerplate Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@mui/material"
+
+function App() {
+
+
+  return (
+    <>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </>
+  )
+}
+
+export default App
